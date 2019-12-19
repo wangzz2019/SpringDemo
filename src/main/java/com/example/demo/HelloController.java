e710e81f9b6909c250e03f581072b896b2715769
wangzz2019 <jack.wang@datadoghq.com>
2019-12-19 16:40:18 +0900

    add log4j2 for log outputing

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class HelloController {
-
+    private final Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String sayHello() {
@@ -16,6 +19,7 @@ public class HelloController {
     private String CombineString(){
         String s1="Hello";
         String s2="This is Jack's Web created by Spring boot";
+        logger.debug("this is a debug message");
         return s1 + ", " + s2;
     }
 }
