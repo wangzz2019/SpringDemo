cbc3b146d14e6a2c41208b3cf8921a9acbb34247
wangzz2019 <jack.wang@datadoghq.com>
2020-09-07 16:34:57 +0900

    add jib setting

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Controller
 public class HelloController {
@@ -20,6 +23,26 @@ public class HelloController {
         String s1="Hello";
         String s2="This is Jack's Web created by Spring boot";
         logger.debug("this is a debug message");
+        logger.info("there is the console message from GET /hello");
+        //System.out.println("there is the console message from GET /hello");
         return s1 + ", " + s2;
+    }
+    @GetMapping("/user/{id}")
+    @ResponseBody
+    public String getUserid(@PathVariable("id") String id){
+        String s1= "user id is: ";
+        String s2=id;
+        logger.info("We have the user id: " + id);
+        return s1+s2;
+    }
+    @RequestMapping(value = "/callothers",method = RequestMethod.GET)
+    @ResponseBody
+    public String callothers(){
+        String endpoint="http://52.196.214.170:5123/test";
+        URI uri= UriComponentsBuilder.fromHttpUrl(endpoint).build().encode().toUri();
+        RestTemplate restTemplate=new RestTemplate();
+        String data=restTemplate.getForObject(uri,String.class);
+        System.out.println(data);
+        return "test";
     }
 }
