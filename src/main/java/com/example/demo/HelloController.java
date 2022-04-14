904a07bf28292d2b9c2b946df085a432ad187012
wangzz2019 <jack.wang@datadoghq.com>
2022-04-14 17:30:00 +0900

    update callothers to a python flask service running on AWS

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.*;
 
 import org.slf4j.Logger;
@@ -12,16 +15,35 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.header.internals.*;
+
+import datadog.trace.api.DDTags;
 import datadog.opentracing.DDTracer;
+import io.opentracing.Tracer;
 import io.opentracing.*;
+import io.opentracing.Scope;
+import io.opentracing.util.GlobalTracer;
 import io.opentracing.util.GlobalTracer;
 import datadog.trace.api.CorrelationIdentifier;
+import datadog.trace.api.Trace;
+import datadog.opentracing.DDTracer;
+
+// Imports the Google Cloud client library
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.Statement;
+
 
 @Controller
 public class HelloController {
     private final Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
     @Autowired
     private KafkaTemplate<String,Object> kafkaTemplate;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String sayHello() {
@@ -35,6 +57,52 @@ public class HelloController {
         //System.out.println("there is the console message from GET /hello");
         return s1 + ", " + s2;
     }
+
+    @RequestMapping(value = "/spanner",method = RequestMethod.GET)
+    @ResponseBody
+    //@Trace(resourceName="/spanner")
+    public String callspanner() {
+        Tracer tracer = GlobalTracer.get();
+        Span span=tracer.buildSpan("callspanner").withTag(DDTags.SERVICE_NAME,"GoogleSpanner")
+                .withTag(DDTags.RESOURCE_NAME,"/spanner")
+                .start();
+        try (Scope scope= tracer.activateSpan(span)){
+            span.setTag("my.tag","value");
+            return spanner();
+        }
+        catch(Exception e){}
+        finally {
+            span.finish();
+        }
+        return "";
+    }
+    private String spanner(){
+        // Instantiates a client
+        SpannerOptions options = SpannerOptions.newBuilder().build();
+        Spanner spanner = options.getService();
+
+        String instanceId = "jacktest";
+        String databaseId = "testdb";
+        try {
+            // Creates a database client
+            DatabaseClient dbClient =
+                    spanner.getDatabaseClient(DatabaseId.of(options.getProjectId(), instanceId, databaseId));
+            // Queries the database
+            ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT * from testtb"));
+
+            System.out.println("\n\nResults:");
+            // Prints the results
+            while (resultSet.next()) {
+                System.out.printf("%d\n\n", resultSet.getLong(0));
+            }
+        } finally {
+            // Closes the client which will free up the resources used
+            spanner.close();
+        }
+        return "call spanner successfully";
+    }
+
+
     @GetMapping("/user/{id}")
     @ResponseBody
     public String getUserid(@PathVariable("id") String id){
@@ -58,10 +126,22 @@ public class HelloController {
         return true;
     }
 
+    //call kafka
+    @GetMapping("/messageandheader/send")
+    @ResponseBody
+    public String kafkaheader(@RequestParam String message,String header){
+        ProducerRecord<String, Object> record = new ProducerRecord<String, Object>("topic-name", null, message);
+        record.headers().add(new RecordHeader("headerkey", header.getBytes()));
+//        record.headers().add(new RecordHeader("type", "record_created".getBytes()));
+//        producer.send(record);
+        kafkaTemplate.send(record);
+        return "send message with header";
+    }
+
     @RequestMapping(value = "/callothers",method = RequestMethod.GET)
     @ResponseBody
     public String callothers(){
-        String endpoint="http://52.196.214.170:5123/test";
+        String endpoint="http://18.180.59.191:8081/test";
         URI uri= UriComponentsBuilder.fromHttpUrl(endpoint).build().encode().toUri();
         RestTemplate restTemplate=new RestTemplate();
         String data=restTemplate.getForObject(uri,String.class);
